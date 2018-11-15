842e627c0b3b319c9a1fcaaccb22a307863c96d3
Anthony <antodu73@hotmail.fr>
2018-11-15 17:13:14 +0100

    Création d'un exemple de base pour gérer une musique
    Création d'une scène exemple pour créer une mapSong

---
+++ Assets/Scripts/MoveMap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMap : MonoBehaviour
+{
+
+    public int BeatPerMinute;
+    public bool hideSubGridAtStart;
+
+    private const float CONSTANT = 0.01665f;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Si on veut cacher la grille au démarrage du projet
+        if (hideSubGridAtStart)
+        {
+            foreach (GameObject subGrid in GameObject.FindGameObjectsWithTag("SubGrid"))
+            {
+                Destroy(subGrid);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector3.back * Time.deltaTime * BeatPerMinute * CONSTANT);
+    }
+}
