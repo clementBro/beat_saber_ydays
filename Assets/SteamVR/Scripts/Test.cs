56f394916cf500c9e111944bc7c27ae22673a379
Jérémy Surieux <jsurieux@gmail.com>
2018-11-28 14:01:59 +0100

    Added test script for collisions

---
+++ Assets/SteamVR/Scripts/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var target = collision.gameObject;
+
+        Debug.Log("Collision détéctée");
+        if (target.name == "Cube")
+        {
+            Destroy(target);
+            Debug.Log("Le GameObject '" + target.name + "' a été détruit");
+        }
+    }
+}
