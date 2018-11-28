70efaab9bccfc3a37ec565a7868a40aa429bb3c8
Jérémy Surieux <jsurieux@gmail.com>
2018-11-28 15:27:31 +0100

    Updated Hit script

--- Assets/Scripts/Hit.cs
+++ Assets/Scripts/Hit.cs
@@ -4,16 +4,6 @@ using UnityEngine;
 
 public class Hit : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     private void OnCollisionEnter(Collision collision)
     {
         var target = collision.gameObject;
