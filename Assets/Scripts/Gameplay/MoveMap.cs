f6e56e07b96fbf3ebd4bd18d66896626499d4bd5
Anthony <antodu73@hotmail.fr>
2019-02-13 14:55:51 +0100

    Sauvegarde music -> Json
    Sauvegarde d'une musique en Json

--- Assets/Scripts/Gameplay/MoveMap.cs
+++ Assets/Scripts/Gameplay/MoveMap.cs
@@ -1,7 +1,11 @@
-﻿using System;
-using System.Collections;
+﻿using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Assets.Scripts.Entities;
 
 public class MoveMap : MonoBehaviour
 {
@@ -67,8 +71,51 @@ public class MoveMap : MonoBehaviour
     private void CreateJson()
     {
         // On récupère les cibles
-        GameObject[] notes = GameObject.FindGameObjectsWithTag("Target");
+        List<GameObject> targets = GameObject.FindGameObjectsWithTag("Target").ToList();
+        targets = targets.OrderBy(note => note.transform.position.z).ToList();
 
+        //Scene.name;
+        DirectoryInfo jsonDirectory = new DirectoryInfo("Assets/JSON/JsonMusic");
 
+        Scene scene = SceneManager.GetActiveScene();
+
+        List<FileInfo> jsonFiles = jsonDirectory.GetFiles().ToList();
+
+        // Si le json existe déjà on le supprime
+        if (jsonFiles.Any(file => file.Name == scene.name + ".json"))
+        {
+            jsonFiles.Where(file => file.Name == scene.name + ".json").First().Delete();
+        }
+
+        // On crée le répertoire
+        FileStream fs = File.Create(jsonDirectory + "/" + scene.name + ".json");
+
+        JsonEntities.DataToSave dataToSave = new JsonEntities.DataToSave();
+        dataToSave.NotesToSave = new List<JsonEntities.NoteToSave>();
+        JsonEntities.NoteToSave noteToSave;
+
+        dataToSave.BPM = BeatPerMinute;
+
+        // On enregistre les infos souhaitées de toutes les cibles
+        foreach (GameObject target in targets)
+        {
+            noteToSave = new JsonEntities.NoteToSave();
+
+            noteToSave.PositionX = target.transform.position.x;
+            noteToSave.PositionY = target.transform.position.y;
+            noteToSave.PositionZ = target.transform.position.z;
+
+            noteToSave.RotationZ = target.transform.rotation.eulerAngles.z;
+
+            dataToSave.NotesToSave.Add(noteToSave);
+        }
+
+        // Transformation de l'objet en Json
+        string jsonMusic = JsonConvert.SerializeObject(dataToSave);
+
+        // Sauvegarde du json dans le fichier
+        UnicodeEncoding uniEncoding = new UnicodeEncoding();
+        fs.Write(uniEncoding.GetBytes(jsonMusic), 0, uniEncoding.GetByteCount(jsonMusic));
+        fs.Close();
     }
 }
